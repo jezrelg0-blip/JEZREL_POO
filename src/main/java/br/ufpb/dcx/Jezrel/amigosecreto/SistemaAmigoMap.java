fe40b77718fa3e972af7dd6bef32ced8da047d59
jezrel.oliveira <jezrelg0@gmail.com>
2026-05-20 12:32:33 -0300

    Fazendo o exercicio 3 :D

---
+++ src/main/java/br/ufpb/dcx/Jezrel/amigosecreto/SistemaAmigoMap.java
@@ -0,0 +1,20 @@
+package br.ufpb.dcx.Jezrel.amigosecreto;
+
+import java.util.HashMap;
+import java.util.Map;
+public class SistemaAmigoMap {
+
+    private Map<Amigo, Amigo> mensagens = new HashMap<>();
+    private Map<Amigo , Amigo> amigos = new HashMap<>();
+
+
+
+
+
+
+
+
+
+
+
+}
